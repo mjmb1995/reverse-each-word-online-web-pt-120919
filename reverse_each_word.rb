ea3f4c28c77ce6918881ac885f39f638d489169a
Mark Bihag <markbihag330@gmail.com>
2020-01-03 03:11:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_array = string.split(" ")
     puts new_array
-    new_array.map do |word|
+    new_array.collect do |word|
       word.reverse
     end
     puts new_array
